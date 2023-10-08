2a08f59c4488b3d2ab2c83a0af950cfd2b94cb03
wfarat <wfarat@gmail.com>
2023-10-08 10:27:56 +0200

    zadanie 37 z rozdzialu 6

---
+++ Zadanie6-37/Zadanie6-37/druga.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "nagl.h"
+using namespace std;
+void druga() {
+	cout << wieksza(5, 4);
+}
---
+++ Zadanie6-37/Zadanie6-37/nagl.h
@@ -0,0 +1,4 @@
+#pragma once
+inline int wieksza(int jeden, int dwa) {
+	return (jeden > dwa) ? jeden : dwa;
+}
