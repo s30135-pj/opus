748d21c145528cb6638038d6cb5b622e8a0207e8
wfarat <wfarat@gmail.com>
2023-10-08 11:31:07 +0200

    Zadanie 49 rozdzial 6

---
+++ Zadanie6-49/Zadanie.cpp
@@ -0,0 +1,34 @@
+﻿// Zadanie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+using namespace std;
+void dwojkowo(int liczba)
+{
+	int reszta = liczba % 2;
+	static int ile = 0;
+	ile++;
+	if (liczba > 1) // warunek zatrzymuj¹cy Cb
+	{
+		dwojkowo(liczba / 2); // wywo³anie rekurencyjne Cc
+	}
+	cout << reszta;
+	ile--;
+	if (ile % 4 == 0) cout << ' ';
+	return;
+}
+int main()
+{
+	dwojkowo(1500);
+}
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
