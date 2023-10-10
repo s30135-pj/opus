87b7a0ca4112b3a6e26b975364e8bc45ca2dbf4b
wfarat <wfarat@gmail.com>
2023-10-10 23:52:04 +0200

    Zadania z rozdzialu 9 o tablicach wielowymiarowych

---
+++ Zadania_roz9_tablice_wielowymiarowe/Zadanie.cpp
@@ -0,0 +1,48 @@
+﻿// Zadanie.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+#include <iostream> 
+#include <cmath>
+using namespace std; 
+float tablica[5][2];
+char t[4][2][2]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p'};
+int t1[3][10];
+double tab[4][5][6];
+int main() {
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 2; j++) {
+			for (int k = 0; k < 2; k++) {
+				cout << t[i][j][k] << endl;
+			}
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 10; j++) {
+			t1[i][j] = pow(j + 1, i + 1);
+		}
+	}
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << t1[j][i] << ' ';
+		}
+		cout << endl;
+	}
+	for (int i = 0; i < 4; i++) {
+		for (int j = 0; j < 5; j++) {
+			for (int k = 0; k < 6; k++) {
+				t[i][j][k] = 0;
+			}
+		}
+	}
+}
+
+
+// Uruchomienie programu: Ctrl + F5 lub menu Debugowanie > Uruchom bez debugowania
+// Debugowanie programu: F5 lub menu Debugowanie > Rozpocznij debugowanie
+
+// Porady dotyczące rozpoczynania pracy:
+//   1. Użyj okna Eksploratora rozwiązań, aby dodać pliki i zarządzać nimi
+//   2. Użyj okna programu Team Explorer, aby nawiązać połączenie z kontrolą źródła
+//   3. Użyj okna Dane wyjściowe, aby sprawdzić dane wyjściowe kompilacji i inne komunikaty
+//   4. Użyj okna Lista błędów, aby zobaczyć błędy
+//   5. Wybierz pozycję Projekt > Dodaj nowy element, aby utworzyć nowe pliki kodu, lub wybierz pozycję Projekt > Dodaj istniejący element, aby dodać istniejące pliku kodu do projektu
+//   6. Aby w przyszłości ponownie otworzyć ten projekt, przejdź do pozycji Plik > Otwórz > Projekt i wybierz plik sln
